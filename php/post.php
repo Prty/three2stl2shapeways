d7d8e98c65fd8c3044400c8a7cc2fe446cc197dd
qantasmz <qanta_shimizu@qanta-mac-2.local>
2014-08-13 16:39:11 -0400

    Securing STL data on server

--- php/post.php
+++ php/post.php
@@ -1,9 +1,46 @@
 <?php
 
+$id = uniqid();
 $stl = $_POST['stl'];
 
-print $stl;
 
+
+$link = mysql_connect("localhost","root", "7m76zz23pu");
+if (!$link) {
+    die('failed' . mysql_error());
+}else{
+$dbname = "3dtweet";
+$tblname="3dt";
+
+
+mysql_select_db($dbname,$link);
+mysql_query('SET NAMES utf8', $link );
+
+
+//$res_up = mysql_query( "DELETE FROM 3dt WHERE id = '$id'", $link);
+	
+
+
+
+
+
+	$file_name = 'data/'.$id.'.stl';
+	touch($file_name);
+
+	chmod( $file_name, 0777 );
+
+	$fp = fopen($file_name, "w");
+	fwrite($fp, $stl);
+	fclose($fp);
+
+$res_m = mysql_query( "INSERT INTO 3dt(id,stl) VALUES ('$id','$file_name')", $link);
+
+
+	mysql_close($link);
+
+}
+
+print $id;
 
 
 
